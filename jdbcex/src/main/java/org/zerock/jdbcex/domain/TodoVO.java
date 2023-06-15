b9255f805f785e4a96bc2c837057c40883667a18
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-06-15 10:28:36 +0900

    23/06/15 10:28 gradle 설정

---
+++ jdbcex/src/main/java/org/zerock/jdbcex/domain/TodoVO.java
@@ -0,0 +1,20 @@
+package org.zerock.jdbcex.domain;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
+
+@Getter  // 읽기 전용으로 많이 사용하므로 추가
+@Builder // 객체 생성 시에 빌더 패턴을 이용하기 위함
+// TodoVO.builder().build()와 같은 형태로 객체 생성 가능
+@ToString
+public class TodoVO {
+    private Long tno;
+    private String title;
+    private LocalDate dueDate;
+    private boolean finished;
+}

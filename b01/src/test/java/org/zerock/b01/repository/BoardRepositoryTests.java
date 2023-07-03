dfb833040d7a5475ee93c34dc2880cca84c4d85c
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-07-03 14:06:05 +0900

    23/07/03 14:05 delete 기능 test

--- b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ b01/src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -4,8 +4,13 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.zerock.b01.domain.Board;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -53,4 +58,30 @@ public class BoardRepositoryTests {
 
         boardRepository.save(board);
     }
+
+    @Test
+    public void testDelete(){
+        Long bno = 1L;
+
+        boardRepository.deleteById(bno);
+    }
+
+    @Test
+    public void testPaging(){
+        // 1page order by bno desc
+        Pageable pageable = PageRequest.of(0,10, Sort.by("bno").descending());
+
+        Page<Board> result = boardRepository.findAll(pageable);
+
+        log.info("total count : " + result.getTotalElements());
+        log.info("total page : " + result.getTotalPages());
+        log.info("page number : " + result.getNumber());
+        log.info("page size : " + result.getSize());
+
+        List<Board> todoList = result.getContent();
+
+        todoList.forEach(board -> log.info(board));
+    }
+
+
 }

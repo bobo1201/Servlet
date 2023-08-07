0f54f135691e30ec97315fc3c97f5211ce2bc0f7
bobo1201 <121841746+bobo1201@users.noreply.github.com>
2023-08-07 10:49:31 +0900

    23/08/07 10:49 JWT 토큰 테스트 완료

--- api01/src/main/java/org/zerock/api01/util/JWTUtil.java
+++ api01/src/main/java/org/zerock/api01/util/JWTUtil.java
@@ -1,10 +1,15 @@
 package org.zerock.api01.util;
 
 import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.time.ZonedDateTime;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 @Component
@@ -14,15 +19,43 @@ public class JWTUtil {
     @Value("${org.zerock.jwt.secret}")
     private String key;
 
+    // jwt 생성시 유효 기간을 days라는 파라미터로 처리했지만 실제로는 plusMinutes()를 이용했으므로 분 단위로 처리됨
+    // 짧은 유효 기간이 테스트 시에 유용하기 때문인데 개발이 완료되면 plusDays()로 변경해 줄 필요가 있음
     public String generateToken(Map<String, Object> valueMap, int days){
         log.info("generateKey..." + key);
 
-        return null;
+        // 헤더 부분
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("typ", "JWT");
+        headers.put("alg", "HS256");
+
+        // payload 부분 설정
+        Map<String, Object> payloads = new HashMap<>();
+        payloads.putAll(valueMap);
+
+        // 테스트 시에는 짧은 유효 기간 (1)이면 1분, (60*24) 하면 유효 기간이 일(day) 단위로 변경됨
+        int time = (60*24) * days; // 테스트는 분단위로 나중에 60 * 24 (일) 단위변경
+
+        // builder를 이용해 Header, Payload 부분을 지정하고 발행 시간과 서명을 이용해 compact()를 수행하면 JWT 문자열 생성됨
+        String jwtStr = Jwts.builder()
+                .setHeader(headers)
+                .setClaims(payloads)
+                .setIssuedAt(Date.from(ZonedDateTime.now().toInstant()))
+                .setExpiration(Date.from(ZonedDateTime.now().plusMinutes(time).toInstant()))
+                .signWith(SignatureAlgorithm.HS256, key.getBytes())
+                .compact();
+
+        return jwtStr;
     }
 
     public Map<String, Object> validateToken(String token) throws JwtException{
 
         Map<String, Object> claim = null;
+
+        claim = Jwts.parser()
+                .setSigningKey(key.getBytes()) // Set Key
+                .parseClaimsJws(token)  // 파싱 및 검증, 실패 시 에러
+                .getBody();
 
         return claim;
     }
--- api01/src/test/java/org/zerock/api01/util/JWTUtilTests.java
+++ api01/src/test/java/org/zerock/api01/util/JWTUtilTests.java
@@ -10,6 +10,7 @@ import java.util.Map;
 @SpringBootTest
 @Log4j2
 public class JWTUtilTests {
+
     @Autowired
     private JWTUtil jwtUtil;
 
@@ -20,5 +21,33 @@ public class JWTUtilTests {
         String jwtStr = jwtUtil.generateToken(claimMap, 1);
 
         log.info(jwtStr);
+    }
+
+    @Test
+    public void testValidate(){
+
+        // 유효 시간이 지난 토큰
+        String jwtStr = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2OTEzNjkyMjUsIm1pZCI6IkFCQ0RFIiwiaWF0IjoxNjkxMzY5MTY1fQ.sLTGC86q_cM3dwhWYu_OaY4ZLS4xon_WPw1MYENFa4s";
+
+        Map<String, Object> claim = jwtUtil.validateToken(jwtStr);
+
+        log.info(claim);
+
+    }
+
+    // JWT 문자열을 생성해서 이를 검증하는 작업을 같이 수행하는 테스트 메소드 작성
+    // mid와 email을 이용해 JWT 문자열을 생성하고 validateToken()을 실행함
+    // validateToken()의 리턴값에는 mid와 email이 그대로 들어있는 것을 확인할 수 있음
+    @Test
+    public void testAll(){
+        String jwtStr = jwtUtil.generateToken(Map.of("mid", "AAAA", "email", "aaaa@bbb.com"), 1);
+
+        log.info(jwtStr);
+
+        Map<String, Object> claim = jwtUtil.validateToken(jwtStr);
+
+        log.info("MID: " + claim.get("mid"));
+
+        log.info("EMAIL: " + claim.get("email"));
     }
 }
